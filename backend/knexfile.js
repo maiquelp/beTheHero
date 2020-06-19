17927d161f7f7952087178de7a55b007d2ad5ce7
Maiquel <maiquelp@gmail.com>
2020-06-19 00:06:28 -0300

    frontend deploy

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -24,29 +24,25 @@ module.exports = {
     useNullAsDefault: true
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'hero',
-      user:     'maiquelp',
-      password: '741536'
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333'
+    baseURL: 'https://warm-temple-94106.herokuapp.com' // 'http://localhost:3333'
 })
 
 export default api;
