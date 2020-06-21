de2524c407b59c5a67ec694169566c060bcbd110
Maiquel <maiquelp@gmail.com>
2020-06-21 16:37:48 -0300

    add screenshots

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'http://localhost:3333' || 'https://warm-temple-94106.herokuapp.com' });
-
-console.log(api.baseURL)
+const api = axios.create({
+    baseURL: process.env.NODE_ENV !== 'production' ? 'http://localhost:3333' : 'https://warm-temple-94106.herokuapp.com' // || 'http://localhost:3333' //'https://warm-temple-94106.herokuapp.com' 
+})
 
 export default api;
